76845538ed2412446b63222345cc3e8ebe4bbbd1
pysolver33 <pysolver33@gmail.com>
2022-03-17 22:28:20 +0800

    debug

--- mvp/load.py
+++ mvp/load.py
@@ -1,28 +1,35 @@
+import os
 import codecs
 import pandas as pd
 import numpy as np
 from deta import Deta
+from dotenv import load_dotenv
+
+
+def parse(entry):
+    if isinstance(entry, float) and np.isnan(entry):
+        return "Nan"
+    elif isinstance(entry, float):
+        return entry
+    elif isinstance(entry, str):
+        return entry.replace(" ", "_")
+    return entry
 
 
 def load_data(db, df):
     for col in df.columns:
         print("Column:", col)
-        print("Entry", type(df[col][0]), df[col][0])
-        for idx, entry in enumerate(df[col]):
-            if isinstance(entry, float) and np.isnan(entry):
-                db.put({col: None}, key=col+"_"+str(idx))
-            elif isinstance(entry, float):
-                db.put({col: entry}, key=col+"_"+str(idx))
-            elif isinstance(entry, str):
-                db.put({col: entry.replace(" ", "_")}, key=col+"_"+str(idx))
+        print("Contemts", df[col].apply(parse).tolist())
+        db.put({col: df[col].apply(parse).tolist()}, key=col)
 
 
 if __name__ == '__main__':
-    deta = Deta("c0crlho4_sW8nwjtvQEHsY2k5HS9B3iHTvA6PQ8c7")
+    load_dotenv()
+    deta = Deta(os.environ.get('key'))
     att_tb = deta.Base("att_tb")
     deal_tb = deta.Base("deal_tb")
     with codecs.open("deal.csv", "r", encoding="utf-8", errors='ignore') as f:
         deal = pd.read_csv(f)
     att = pd.read_csv("companyatt.csv")
     load_data(att_tb, att)
-    # load_data(deal_tb, deal)
+    load_data(deal_tb, deal)
--- mvp/main.py
+++ mvp/main.py
@@ -1,13 +1,13 @@
-import json
 import os
 import pandas as pd
 import numpy as np
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from deta import Deta
 from dotenv import load_dotenv
+from collections import ChainMap
 
 load_dotenv()
 app = FastAPI()
@@ -18,17 +18,52 @@ deta = Deta(os.environ.get('key'))
 db = deta.Base("mvpDB")
 
 
-# TO DO finish loading template
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/")
+def home():
+    return "Hello World"
 
 
-@app.post("/read/{table}/{col}")
-async def read(table: str, col: str):
-    db = deta.Base(table)
-    r = db.read(col)
-    return r if r else json.dumps({'error': 'No data'})
+@app.get("/form")
+def form_post(request: Request):
+    result = "Please query att_tb | deal_tb"
+    return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
+
+
+@app.post("/form")
+async def read(
+    request: Request,
+    table: str = Form("deal_tb"),
+    col: str = Form("deal_id"),
+    row: str = Form("1"),
+    agg: str = Form("agg")
+):
+    tables = ("att_tb", "deal_tb")
+    aggs = ('value_counts', 'describe')
+    df = "Invalid Query"
+
+    if table in tables:
+        db = deta.Base(table)
+
+    if db and col == "all" and row == "all":
+        result = db.fetch()
+        df = pd.DataFrame(dict(ChainMap(*result.items)))
+        if agg in aggs:
+            return templates.TemplateResponse('form.html', context={'request': request, 'result': df.agg(agg).to_html()})
+        return templates.TemplateResponse('form.html', context={'request': request, 'result': df.to_html()})
+
+    if db and col and row.isdigit():
+        q = db.get(key=col)
+        if q and int(row) < len(q[col]):
+            df = pd.DataFrame(q)[:int(row)]
+            if agg in aggs:
+                return templates.TemplateResponse('form.html', context={'request': request, 'result': df.agg(agg).to_html()})
+            return templates.TemplateResponse('form.html', context={'request': request, 'result': df.to_html()})
+
+    if db and db.get(key=col):
+        df = pd.DataFrame(db.get(key=col))
+        return templates.TemplateResponse('form.html', context={'request': request, 'result': df.to_html()})
+
+    return templates.TemplateResponse('form.html', context={'request': request, 'result': df})
 
 
 if __name__ == '__main__':
